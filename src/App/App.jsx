c609c1e9603665ece77ef3295f0b16f7034b424d
dlmarques <daniel.marquesedigital@gmail.com>
2022-05-17 01:24:59 +0100

    App

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
     const [closeSide, setCloseSide] = useState(false)
   return (
     <>
-    <div className="main-container">
+    <div className="main-container-app">
         <div className="sidebar-app"> {/*{!closeSide ? <SideBar setCloseSide={setCloseSide} closeSide={closeSide}/> : <SmallSideBar/> }*/} <SideBar setCloseSide={setCloseSide} closeSide={closeSide}/> </div>
         <div className="content-container">
             <div className="topbar"> <TopBar setCloseSide={setCloseSide} closeSide={closeSide} /> </div>
